9496c460bdc3989d5e001508b5dd7e307e16b5d4
Papa Deere <papadeere5150@gmail.com>
2018-03-15 11:17:19 -0700

    initial commit

---
+++ script/scrapeNYT.js
@@ -0,0 +1,34 @@
+const cheerio = require('cheerio')
+var request = require('request');
+
+//hit nyt scrape data
+var scrape = function(){
+    return request("http://www.nytimes.com", function (error, response, body) {
+    
+    var articles = [];
+
+    const $ = cheerio.load(body)
+    $(".theme-summary").each(function (i, article){
+      console.log($(this).children(".story-heading").children("a").attr("href"))
+      var summary = $(this).children(".summary").text().trim();
+      var headline = $(this).children(".story-heading").children("a").text().trim();
+      var url = $(this).children(".story-heading").children("a").attr("href");
+
+      var data = {
+        summary: summary,
+        headline: headline,
+        url: url
+      };
+      
+      articles.push(data);
+    });
+    return articles;
+  });
+};
+
+
+module.exports = scrape;
+// laod it into cheerio
+// take all data push it into and array
+//return that array from this file so we can store it into our db
+
